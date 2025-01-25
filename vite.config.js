36486c77fc5d665a21210cb0657e9cdd89f63756
JackWhiters <73758793+JackWhiters@users.noreply.github.com>
2025-01-25 20:35:20 +0700

    fix config.js

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,31 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { visualizer } from 'rollup-plugin-visualizer';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
   plugins: [react(), visualizer()],
   resolve: {
     alias: {
-      "@": "/src", // Optional alias for `src`
+      "@": "/src", // Opsional, untuk mempermudah import dari `src`
     },
   },
   build: {
-    minify: 'esbuild',
-    cssMinify: 'lightningcss',
+    minify: "esbuild",
+    cssMinify: "lightningcss",
     rollupOptions: {
-      treeshake: 'recommended',
-      external: [
-        "/assets/imgs/404-light.webp",
-        "/assets/imgs/404-dark.webp",
-        "/assets/imgs/logo-niqi.webp",
-      ],
+      treeshake: "recommended",
       output: {
         manualChunks: {
-          editorJs: ['@editorjs/editorjs'],
-          firebaseApp: ['firebase/app'],
-          firebaseAuth: ['firebase/auth'],
-          framerMotion: ['framer-motion'],
-          axios: ['axios'],
-          reactDom: ['react-dom/client'],
+          editorJs: ["@editorjs/editorjs"],
+          firebaseApp: ["firebase/app"],
+          firebaseAuth: ["firebase/auth"],
+          framerMotion: ["framer-motion"],
+          axios: ["axios"],
+          reactDom: ["react-dom/client"],
         },
       },
     },
-    assetsInclude: ["**/*.webp"], // Explicitly include webp files
+    // Pastikan aset seperti .webp dikenali
+    assetsInclude: ["**/*.webp"],
   },
 });
