49ded58170fa85e7b32fe4d32a273493b0baa535
JackWhiters <73758793+JackWhiters@users.noreply.github.com>
2025-01-25 20:48:07 +0700

    vite.config.js ganti ke public

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react(), visualizer()],
   resolve: {
     alias: {
-      "@": "/src", // Opsional, untuk mempermudah import dari `src`
+      "@": "/public", // Opsional, untuk mempermudah import dari `src`
     },
   },
   build: {
